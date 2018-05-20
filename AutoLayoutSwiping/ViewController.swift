a86e509392fc16a720db9bc0c802e49070dff49a
Patrik Kemeny <patrikkemeny@Patriks-MacBook-Pro.local>
2018-05-20 14:37:59 +1000

    pageControll and stackView

--- AutoLayoutSwiping/ViewController.swift
+++ AutoLayoutSwiping/ViewController.swift
@@ -41,6 +41,35 @@ class ViewController: UIViewController {
     }()
     
     
+    private let previousButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("PREV", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), for: .normal)
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("NEXT", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let pinkColor = UIColor(red: 233/255, green: 68/255, blue: 133/255, alpha: 1)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+         button.setTitleColor(pinkColor, for: .normal)
+        return button
+    }()
+    
+    private let pageControl:UIPageControl  = {
+        let pc  = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 4
+        pc.currentPageIndicatorTintColor = .red
+        pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        // App entry point
@@ -49,12 +78,45 @@ class ViewController: UIViewController {
         //   imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50) programitically positioning
         
         //view.addSubview(bearImgeView)
-        view.addSubview(descriptionTextView )
+        view.addSubview(descriptionTextView)
+        
+        setupBottomControls()
+        
         setupLayout()
        
      
         
         
+    }
+    
+    fileprivate func setupBottomControls(){
+        view.addSubview(previousButton)
+      //  previousButton.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+       // previousButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50 ) //just to see that it exist
+       
+//
+//        let yellowView = UIView()
+//        yellowView.backgroundColor = #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)
+//
+        let greenView = UIView()
+        greenView.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        
+//        let blueView = UIView()
+//        blueView.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+//
+        let bottomsControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomsControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomsControlsStackView.distribution = .fillEqually
+        bottomsControlsStackView.axis = .horizontal
+        
+        view.addSubview(bottomsControlsStackView)
+        
+        NSLayoutConstraint.activate([
+            bottomsControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor ),
+            bottomsControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            bottomsControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            bottomsControlsStackView.heightAnchor.constraint(equalToConstant: 50)
+            ])// if i am using this i do not need to use .isActive when i am calling the contraints
     }
 
     private func setupLayout() {
@@ -97,6 +159,9 @@ class ViewController: UIViewController {
 
     }
 
+    
+    
+   
 }
 
 
